1d30af207a18d821eebe1373cc6c0e6cddd0e82d
LovroLulic <lovro.lulic1905@gmail.com>
2025-10-28 16:40:29 +0100

    Update Route.java

--- entities/Route.java
+++ entities/Route.java
@@ -11,35 +11,64 @@ public class Route {
     private String pocetnastanica;
     private String krajnastanica;
     private BigDecimal kilometers;
+    private CijenaKarte cjenik;
 
-    public Route(Vehicle vehicle, LocalDate date, String time, String pocetnastanica,String krajnastanica, BigDecimal kilometers) {
+    private Route(Vehicle vehicle, LocalDate date, String time, String pocetnastanica,String krajnastanica, BigDecimal kilometers, CijenaKarte cjenik) {
         this.vehicle = vehicle;
         this.date = date;
         this.time = time;
         this.pocetnastanica = pocetnastanica;
         this.krajnastanica = krajnastanica;
         this.kilometers = kilometers;
+        this.cjenik=cjenik;
     }
 
-
-    public void setVehicle(Vehicle vehicle) {
-        this.vehicle = vehicle;
+    public static Builder builder(Vehicle vehicle, LocalDate date){
+        return new Builder(vehicle,date);
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public static class Builder{
+        private final Vehicle vehicle;
+        private final LocalDate date;
+        private String time="00:00";
+        private String pocetnastanica="Nepoznata";
+        private String krajnastanica="Nepoznata";
+        private BigDecimal kilometers=BigDecimal.ZERO;
+        private CijenaKarte cjenik;
+
+        private Builder(Vehicle vehicle, LocalDate date){
+            this.vehicle=vehicle;
+            this.date=date;
+        }
+
+        public Builder time(String time){
+            this.time=time;
+            return this;
+        }
+
+        public Builder pocetnastanica(String pocetnastanica){
+            this.pocetnastanica=pocetnastanica;
+            return this;
+        }
+
+        public Builder krajnastanica(String krajnastanica){
+            this.krajnastanica=krajnastanica;
+            return this;
+        }
+
+        public Builder kilometers(BigDecimal kilometers){
+            this.kilometers=kilometers;
+            return this;
+        }
+
+        public Builder cjenik(CijenaKarte cjenik){
+            this.cjenik=cjenik;
+            return this;
+        }
+        public Route build(){
+            return new Route(vehicle,date,time,pocetnastanica,krajnastanica,kilometers,cjenik);
+        }
     }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
-
-    public void setPocetnastanica(String pocetnastanica) {this.pocetnastanica = pocetnastanica;}
-
-    public void setKrajnastanica(String krajnastanica) {this.krajnastanica = krajnastanica;}
-
-    public void setKilometers(BigDecimal kilometers) { this.kilometers = kilometers; }
-
     
     public Vehicle getVehicle() {
         return vehicle;
@@ -60,9 +89,25 @@ public class Route {
     public BigDecimal getKilometers() { return kilometers; }
 
     public void ispis(){
+        System.out.println("----------------------------------");
         System.out.println("Smjer " + pocetnastanica+" - "+krajnastanica);
         System.out.println("Datum: "+date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))+ " u "+time+" sati.");
         System.out.println("Udaljenost: "+kilometers+" km.");
+        System.out.println();
+        boolean nocna=CijenaKarte.jeNocna(time);
+        if(nocna){
+            System.out.println("⚠\uFE0F NOCNA VOZNJA (23:30 - 05:30)");
+            System.out.println("Cijena: "+cjenik.nocnaCijena()+"€");
+            System.out.println("(Studentski popust: "+cjenik.studentskaNocna()+"€, umirovljenicki popust: "+cjenik.umirovljenickaNocna()+"€)");
+            System.out.println();
+        }
+        else{
+            System.out.println("Cijena: "+cjenik.cijena()+"€");
+            System.out.println("(Studentski popust: "+cjenik.studentskaCijena()+"€, umirovljenicki popust: "+cjenik.umirovljenickaCijena()+"€)");
+            System.out.println();
+        }
         vehicle.ispis();
+
+        System.out.println("----------------------------------");
     }
 }
